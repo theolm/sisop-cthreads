67641820a2f238d340999dbcd69e188bb9bd5477
Theo <theolm.mota@gmail.com>
2019-04-07 16:25:40 -0300

    cidentify ok
    cidentify done with makefile and test.

--- cthread/src/lib.c
+++ cthread/src/lib.c
@@ -4,40 +4,49 @@
 #include "../include/support.h"
 #include "../include/cthread.h"
 #include "../include/cdata.h"
+#define FUNCTION_SUCCESS 0
+#define FUNCTION_ERROR -1
+#define FUNCTION_NOT_IMPLEMENTED -1
 
 
 int ccreate (void* (*start)(void*), void *arg, int prio) {
-    printf("teste");
-	return -1;
+	return FUNCTION_NOT_IMPLEMENTED;
 }
 
 int csetprio(int tid, int prio) {
-	return -1;
+	return FUNCTION_NOT_IMPLEMENTED;
 }
 
 int cyield(void) {
-	return -1;
+	return FUNCTION_NOT_IMPLEMENTED;
 }
 
 int cjoin(int tid) {
-	return -1;
+	return FUNCTION_NOT_IMPLEMENTED;
 }
 
 int csem_init(csem_t *sem, int count) {
-	return -1;
+	return FUNCTION_NOT_IMPLEMENTED;
 }
 
 int cwait(csem_t *sem) {
-	return -1;
+	return FUNCTION_NOT_IMPLEMENTED;
 }
 
 int csignal(csem_t *sem) {
-	return -1;
+	return FUNCTION_NOT_IMPLEMENTED;
 }
 
 int cidentify (char *name, int size) {
-	strncpy (name, "Sergio Cechin - 2017/1 - Teste de compilacao.", size);
-	return 0;
+    char members[] = "Theodoro Loureiro Mota - 173230\nRui Cardozo - 120211\nFabio Petkowicz - 118627";
+    int members_size = sizeof(members) / sizeof(members[0]);
+
+    if(size < members_size) {
+        return FUNCTION_ERROR;
+    } else {
+        strncpy (name, members, size);
+        return FUNCTION_SUCCESS;
+    }
 }
 
 
---
+++ cthread/teste/names.c
@@ -0,0 +1,29 @@
+//
+// Created by theo on 07/04/19.
+//
+
+#include "../include/support.h"
+#include "../include/cthread.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    printf("Iniciou main, teste de nomes dos integrantes\n\n");
+
+    char integrantes[100];
+
+    int result = cidentify(integrantes, 100);
+
+    if(result == 0) {
+        printf("%s\n", integrantes);
+    } else if (result == -1){
+        printf("Erro ao executar cidentify. Verificar tamanho do array de destino.");
+    } else if(result == -9) {
+        printf("Funcao nao implementada.");
+    } else {
+        printf("Erro nao especificado.");
+    }
+
+    printf("\n%d\n", result);
+
+}
