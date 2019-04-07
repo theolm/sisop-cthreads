cc0f58ea974ddf4a459fef8641f9abb7a1e50f8c
Theo <theolm.mota@gmail.com>
2019-04-07 17:28:16 -0300

    Makefile clean fix

--- cthread/src/lib.c
+++ cthread/src/lib.c
@@ -4,18 +4,28 @@
 #include "../include/support.h"
 #include "../include/cthread.h"
 #include "../include/cdata.h"
+
 #define FUNCTION_SUCCESS 0
 #define FUNCTION_ERROR -1
 #define FUNCTION_NOT_IMPLEMENTED -1
+#define PRIORITY_HIGH 0
+#define PRIORITY_MEDIUM 1
+#define PRIORITY_LOW 2
+#define TID_MAIN_THREAD 0 /*A main thread tem prioridade baixa*/
 
 
 int ccreate (void* (*start)(void*), void *arg, int prio) {
 	return FUNCTION_NOT_IMPLEMENTED;
 }
 
+/**
+ * tid: Na versão 2019/01, deixar sempre esse campo
+ * como NULL
+ **/
 int csetprio(int tid, int prio) {
 	return FUNCTION_NOT_IMPLEMENTED;
 }
+
 
 int cyield(void) {
 	return FUNCTION_NOT_IMPLEMENTED;
