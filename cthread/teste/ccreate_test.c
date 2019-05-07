fe85bea5152c24410e5773455b67db2b56bfa6b1
Theo <theolm.mota@gmail.com>
2019-05-07 10:08:57 -0700

    new test

--- cthread/teste/ccreate_test.c
+++ cthread/teste/ccreate_test.c
@@ -14,29 +14,35 @@ int func_2_tid = -1;
 void* func1(void *arg) {
     int i = 0;
     for (i =1; i <=1000; i++ ) {
-        printf("%d\n", i);
+        printf("A\n", i);
         if(i%5 == 0) {
-            printf("\nYIELD\n");
             cyield();
         }
     }
 }
 
 void* func2(void *arg) {
-    int i;
-    cjoin(func_1_tid);
-    for (i =1; i <=20; i++ ) {
-        printf("b\n");
+    int i = 0;
+    for (i =1; i <=1000; i++ ) {
+        printf("B\n", i);
+        if(i%5 == 0) {
+            cyield();
+        }
     }
 }
+
+
+
 
 csem_t csem;
 int main(int argc, char *argv[]) {
     int a,b;
     func_1_tid = ccreate(func1, (void *)&a, 2);
+    func_2_tid = ccreate(func2, (void *)&b, 2);
     cjoin(func_1_tid);
-    csem = *(csem_t *)malloc(sizeof(csem_t));
-    csem_init(&csem, 5);
+    cjoin(func_2_tid);
+    //csem = *(csem_t *)malloc(sizeof(csem_t));
+    //csem_init(&csem, 5);
 
     printf("\n-------------- FINAL DA MAIN -----------------\n");
     exit(0);
